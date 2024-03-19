e3e5a0733fea3a07f0ae2a58aef92e750125f64e
techmefr <compigni.gaetan@gmail.com>
2024-03-19 09:56:19 +0100

    fix exo3 and delete punctuation

--- assets/scripts/exo3.js
+++ assets/scripts/exo3.js
@@ -1,1 +1,14 @@
-console.log(getLongestWord("Du sublime au ridicule il n'y a qu'un pas.")); // 8
+function getLongestWord(sentence) {
+  let words = sentence.replace(/[^a-zA-Z\s]/g, "").split(/\s+/);
+  let longestWord = "";
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+
+  return longestWord;
+}
+
+console.log(getLongestWord("Du sublime, au ridicule il n'y a qu'un pas."));
