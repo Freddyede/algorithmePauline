895a8a385d80492f1066a5788df7d047f24a425d
Franck <136079691+Freddyede@users.noreply.github.com>
2024-03-19 09:32:31 +0100

    Update exo2

---
+++ assets/scripts/exo2.js
@@ -0,0 +1,26 @@
+console.log(
+    scanAndFind(
+        [
+            { prenom: "Tom", nom: "Durand" },
+            { prenom: "Juliette", nom: "Garcia" },
+            { prenom: "Jean", nom: "Lafite" },
+            { prenom: "Lucien", nom: "Lafite" },
+            { prenom: "Lucien", abc: "Lafite" }
+        ],
+        { nom: "Lafite" }
+    )
+);
+// Résultat -> [{ prenom: 'Jean', nom: 'Lafite' },{ prenom: 'Lucien', nom: 'Lafite' }]
+
+console.log(
+    scanAndFind(
+        [
+            { prenom: "Joe", code: 65421 },
+            { prenom: "John", id: 556487 },
+            { prenom: "Melinda", identification: 54834 },
+            { prenom: "Rose", id: 68784 },
+            { prenom: "Jack", id: 41244 }
+        ],
+        { id: 556487 }
+    )
+);
